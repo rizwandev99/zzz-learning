ac795fae59a70341126dcf1c5df739c02da36ed4
Md Rizwan <rizwandev99@gmail.com>
2024-07-06 21:37:32 +0530

    CreateToDo

---
+++ 100xdev/new-year-app/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+export function CreateTodo() {
+  return (
+    <>
+      <input type="text" placeholder="title"></input>
+      <br />
+      <input type="text" placeholder="description"></input>
+      <br />
+      <button>Add a toDo</button>
+    </>
+  );
+}
