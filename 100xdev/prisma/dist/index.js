f517844376363da342902119ea3715b3e85420f6
Rizwan <rizwandev99@gmail.com>
2024-07-25 11:19:26 +0530

    started cloudflare server

--- 100xdev/prisma/dist/index.js
+++ 100xdev/prisma/dist/index.js
@@ -1,40 +1,11 @@
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
-// interface UpdateParams {
-//   firstName: string;
-//   lastName: string;
-// }
-// async function updateUser(
-//   username: string,
-//   { firstName, lastName }: UpdateParams
-// ) {
-//   try {
-//     const res = await prisma.user.update({
-//       where: { email: username },
-//       data: {
-//         firstName,
-//         lastName,
-//       },
-//     });
-//     console.log(res);
-//   } catch (error) {
-//     console.error("Error updating user:", error);
-//   } finally {
-//     await prisma.$disconnect();
-//   }
-// }
-// // Call the function
-// updateUser("a1", { firstName: "rahul", lastName: "tyagi" });
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-async function updateUser(username, { firstName, lastName }) {
-    const res = await prisma.user.update({
-        where: { email: username },
-        data: {
-            firstName,
-            lastName,
+async function getUser(username) {
+    const res = await prisma.user.findUnique({
+        where: {
+            email: username,
         },
     });
     console.log(res);
 }
-updateUser("a1", { firstName: "mr", lastName: "md" });
+getUser("a1");
--- 100xdev/prisma/src/index.ts
+++ 100xdev/prisma/src/index.ts
@@ -1,56 +1,13 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient();
-
-// interface UpdateParams {
-//   firstName: string;
-//   lastName: string;
-// }
-
-// async function updateUser(
-//   username: string,
-//   { firstName, lastName }: UpdateParams
-// ) {
-//   try {
-//     const res = await prisma.user.update({
-//       where: { email: username },
-//       data: {
-//         firstName,
-//         lastName,
-//       },
-//     });
-//     console.log(res);
-//   } catch (error) {
-//     console.error("Error updating user:", error);
-//   } finally {
-//     await prisma.$disconnect();
-//   }
-// }
-
-// // Call the function
-// updateUser("a1", { firstName: "rahul", lastName: "tyagi" });
-
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-interface UpdateParams {
-  firstName: string;
-  lastName: string;
-}
-
-async function updateUser(
-  username: string,
-  { firstName, lastName }: UpdateParams
-) {
-  const res = await prisma.user.update({
-    where: { email: username },
-    data: {
-      firstName,
-      lastName,
+async function getUser(username: string) {
+  const res = await prisma.user.findUnique({
+    where: {
+      email: username,
     },
   });
   console.log(res);
 }
-
-updateUser("a1", { firstName: "mr", lastName: "md" });
+getUser("a1");
